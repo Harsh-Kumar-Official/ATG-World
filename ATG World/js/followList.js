1f08a757a4a67646c3b208a2985eb5066bd1d6d8
Harsh Kumar <kumarharshsingh.8976@gmail.com>
2024-06-30 06:44:17 +0530

    Delete ATG World directory

--- ATG World/js/followList.js
+++
@@ -1,9 +0,0 @@
-function toggleFollow(button) {
-    if (button.classList.contains("followed")) {
-        button.classList.remove("followed");
-        button.textContent = "Follow";
-    } else {
-        button.classList.add("followed");
-        button.textContent = "Followed";
-    }
-}
